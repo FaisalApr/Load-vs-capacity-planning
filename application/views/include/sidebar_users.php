0cf25de9d7e5f0d99baed6399c0144dea850cec5
FaisalApr <45162241+FaisalApr@users.noreply.github.com>
2019-08-01 11:02:03 +0700

    Update sidebar_users.php

--- application/views/include/sidebar_users.php
+++ application/views/include/sidebar_users.php
@@ -17,8 +17,8 @@
 
 	<div class="left-side-bar">
 		<div class="brand-logo">
-			<a href="<?php echo site_url('Dasboard') ?>">
-				<img src="<?php echo base_url() ?>assets/vendors/images/SAI.png" alt="">
+			<a href="<?php echo site_url('') ?>">
+				<img src="<?php echo base_url() ?>assets/vendors/images/SAI.PNG" alt="">
 			</a>
 		</div>
 		<div class="menu-block customscroll">
@@ -42,75 +42,6 @@
 							<span class="fa fa-times"></span><span class="mtext">DEFECT</span>
 						</a>
 					</li>
-
-					<li class=" <?php echo $this->uri->segment(1) == 'Summary' ? 'aktip': '' ?>">
-						<a href="<?php echo site_url() ?>/Summary" class="dropdown-toggle no-arrow"> 
-							<i class="icon-copy fa fa-line-chart" aria-hidden="true"></i><span class="mtext">SUMMARY</span>
-						</a>
-					</li>  
-
-					<li class="dropdown <?php echo $this->uri->segment(1) == 'directlabor' ? 'aktip': '' ?> <?php echo $this->uri->segment(1) == 'indirectlabor' ? 'aktip': '' ?>">
-						<a href="javascript:;" class="dropdown-toggle">
-							<i class="icon-copy fa fa-group" aria-hidden="true"></i><span class="mtext">MAN POWER</span>
-						</a>
-						<ul class="submenu">
-							<li><a href="<?php echo site_url() ?>/directlabor">Direct</a></li>
-							<li><a href="<?php echo site_url() ?>/indirectlabor">Indirect</a></li>
-						</ul>
-					</li>
-					
-					<li class=" <?php echo $this->uri->segment(1) == 'Export' ? 'aktip': '' ?>  <?php echo $sesi['level']!=1 ? 'hidenn': '' ?>">
-						<a href="<?php echo site_url() ?>/Export" class="dropdown-toggle no-arrow"> 
-							<i class="icon-copy fa fa-file-archive-o" aria-hidden="true"></i><span class="mtext">Export</span>
-						</a>
-					</li> 
-
-					<!-- FITUR EXTRA ADMIN -->
-					<hr class="<?php echo $sesi['level']!=1 ? 'hidenn': '' ?>"> 
-					
-					<li class=" <?php echo $this->uri->segment(1) == 'supervisor' ? 'aktip': '' ?>  <?php echo $sesi['level']!=1 ? 'hidenn': '' ?>">
-						<a href="<?php echo site_url() ?>/supervisor" class="dropdown-toggle no-arrow"> 
-							<i class="icon-copy fa fa-id-badge" aria-hidden="true"></i><span class="mtext">Supervisor</span>
-						</a>
-					</li>
-					
-					<li class=" <?php echo $this->uri->segment(1) == 'Users' ? 'aktip': '' ?>  <?php echo $sesi['level']!=1 ? 'hidenn': '' ?>">
-						<a href="<?php echo site_url() ?>/Users" class="dropdown-toggle no-arrow"> 
-							<i class="icon-copy fa fa-id-card-o" aria-hidden="true"></i><span class="mtext">Users</span>
-						</a>
-					</li> 
-
-					<li class=" <?php echo $this->uri->segment(1) == 'carline' ? 'aktip': '' ?>  <?php echo $sesi['level']!=1 ? 'hidenn': '' ?>">
-						<a href="<?php echo site_url() ?>/carline" class="dropdown-toggle no-arrow"> 
-							<i class="icon-copy fa fa-car" aria-hidden="true"></i><span class="mtext">Carline</span>
-						</a>
-					</li> 
-
-					<li class=" <?php echo $this->uri->segment(1) == 'LineManager' ? 'aktip': '' ?>  <?php echo $sesi['level']!=1 ? 'hidenn': '' ?>">
-						<a href="<?php echo site_url() ?>/LineManager" class="dropdown-toggle no-arrow"> 
-							<i class="icon-copy fa fa-code-fork" aria-hidden="true"></i><span class="mtext">Line Manager</span>
-						</a>
-					</li> 
-
-					<br><br>
-					<hr>
-					<li class=" <?php echo $this->uri->segment(1) == 'assycode' ? 'aktip': '' ?>  <?php echo $sesi['level']!=1 ? 'hidenn': '' ?>">
-						<a href="<?php echo site_url() ?>/assycode" class="dropdown-toggle no-arrow"> 
-							<i class="icon-copy fa fa-briefcase" aria-hidden="true"></i><span class="mtext">Assembly</span>
-						</a>
-					</li> 
-					
-					<li class=" <?php echo $this->uri->segment(1) == 'errcode' ? 'aktip': '' ?>  <?php echo $sesi['level']!=1 ? 'hidenn': '' ?>">
-						<a href="<?php echo site_url() ?>/errcode" class="dropdown-toggle no-arrow"> 
-							<i class="icon-copy fa fa-arrow-circle-down" aria-hidden="true"></i><span class="mtext">Downtime</span>
-						</a>
-					</li>
-					<li class=" <?php echo $this->uri->segment(1) == 'Line' ? 'aktip': '' ?>  <?php echo $sesi['level']!=1 ? 'hidenn': '' ?>">
-						<a href="<?php echo site_url() ?>/Line" class="dropdown-toggle no-arrow"> 
-							<i class="icon-copy fa fa-road" aria-hidden="true"></i>Line</span>
-						</a>
-					</li>
-
 				</ul>
 			</div>
 		</div>
